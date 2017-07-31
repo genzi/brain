bf0dbe283424341589e8fde555c1fda401b7444e
Lukasz Gesieniec <lukasz@micromex.com.pl>
2017-07-31 12:20:59 +0200

    More tests + reference

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # brain
 Very simple neural network in golang
+
+# Reference:
+https://github.com/stevenmiller888/go-mind
+
+https://github.com/goml/gobrain
--- brain_test.go
+++ brain_test.go
@@ -1,6 +1,7 @@
 package brain
 
 import (
+	"log"
 	"testing"
 )
 
@@ -37,4 +38,124 @@ func TestXor(t *testing.T) {
 		}
 	}
 
+}
+
+func TestAnd(t *testing.T) {
+	patterns := [][][]float64{
+		{{0, 0}, {0}},
+		{{0, 1}, {0}},
+		{{1, 0}, {0}},
+		{{1, 1}, {1}},
+	}
+
+	gobrain := New(2, 3, 1)
+
+	gobrain.Train(patterns, 100000, 0.2, 0.1)
+
+	for _, p := range patterns {
+		result := gobrain.Process(p[0])
+		result[0] += 0.5 // +0.5 for round
+		if int(result[0]) != int(p[1][0]) {
+			t.Error("Xor error!", int(result[0]), int(p[1][0]))
+		}
+	}
+
+}
+
+/////////////////////////////////helping functions//////////////////////////////
+func character(chars string) []float64 {
+	flt := make([]float64, len(chars))
+	for i := 0; i < len(chars); i++ {
+		if chars[i] == '#' {
+			flt[i] = 1.0
+		} else { // if '.'
+			flt[i] = 0.0
+		}
+	}
+	return flt
+}
+
+func mapletter(letter byte) []float64 {
+	if letter == 'a' {
+		return []float64{0.1}
+	}
+	if letter == 'b' {
+		return []float64{0.3}
+	}
+	if letter == 'c' {
+		return []float64{0.5}
+	}
+	return []float64{0.0}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+func TestRecognizeCharacter(t *testing.T) {
+	var (
+		a = character(
+			".#####." +
+				"#.....#" +
+				"#.....#" +
+				"#######" +
+				"#.....#" +
+				"#.....#" +
+				"#.....#")
+		b = character(
+			"######." +
+				"#.....#" +
+				"#.....#" +
+				"######." +
+				"#.....#" +
+				"#.....#" +
+				"######.")
+		c = character(
+			"#######" +
+				"#......" +
+				"#......" +
+				"#......" +
+				"#......" +
+				"#......" +
+				"#######")
+	)
+
+	gobrain := New(49, 15, 1)
+
+	gobrain.Train([][][]float64{
+		{c, mapletter('c')},
+		{b, mapletter('b')},
+		{a, mapletter('a')},
+	}, 1000000, 0.2, 0.1)
+
+	result := gobrain.Process(
+		character(
+			"######." +
+				"#.....#" +
+				"#.....#" +
+				"#######" +
+				"#.....#" +
+				"#.....#" +
+				"#.....#"))
+	log.Println("Should be around 0.1", result)
+
+	result = gobrain.Process(
+		character(
+			"#######" +
+				"#.....#" +
+				"#.....#" +
+				"######." +
+				"#.....#" +
+				"#.....#" +
+				"######."))
+	log.Println("Should be around 0.3", result)
+
+	result = gobrain.Process(
+		character(
+			"#######" +
+				"#......" +
+				"#......" +
+				"#......" +
+				"#......" +
+				"##....." +
+				"#######"))
+	log.Println("Should be around 0.5", result)
 }
